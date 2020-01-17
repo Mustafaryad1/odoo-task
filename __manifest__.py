314f6de9046e2c5da5354c0f306456ee6910e281
Mustafaryad1 <mustafariad03@gmail.com>
2020-01-17 22:32:57 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Product && Product Category',
+    'description': 'Product && Product Category',
+    'author': 'Mustafa',
+    'depends': ['base', 'product'],
+    'data': [
+        'views/product_view.xml',
+        'views/product_category_view.xml',
+    ],
+}
